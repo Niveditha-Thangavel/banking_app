38376b8964d7d29ddc3bee1a7e612c9c9857af64
Niveditha-Thangavel <nivedithathangavel7@gmail.com>
2025-12-01 14:19:05 +0530

    calculate.py modified 1

--- calculate.py
+++ calculate.py
@@ -1,31 +1,32 @@
 import json 
 import input
+from datetime import datetime
 
 with open("customer_data.json") as f:
     statement = json.load(f)
 
-
-
 def calculate(statement):
-    avg_month_spend = 0
-    avg_month_income = 0
+    income = 0
+    spend = 0
+    month_spend = 0
+    month_income = 0
     no_of_transaction = len(statement["transactions"])
     for trans in statement["transactions"]:
         if trans["type"] == "credit":
-            avg_month_income+= trans["amount"]
+            month_income+= trans["amount"]
+            income+=1
         else:
-            avg_month_spend+= trans["amount"]
-    total_transacrtion_amount =  avg_month_spend+avg_month_income 
+            month_spend+= trans["amount"]
+            spend+=1
+    avg_month_income = month_income/income
+    avg_month_spend = month_spend/spend
+
+    total_transacrtion_amount =  month_spend+ month_income 
     avg_spend_ratio = (avg_month_spend/avg_month_income) * 100
-    avg_transaction_amount= total_transacrtion_amount
-    for trans in statement["transaction"]:
+    avg_transaction_amount= total_transacrtion_amount / (income+spend)
+    for trans in statement["transactions"]:
         mean_diff = avg_transaction_amount - trans["amount"]
-        
 
-   
+    print(avg_month_income,avg_month_spend,avg_spend_ratio,avg_transaction_amount,mean_diff)
 
-
-
-        
-
-    
+calculate(statement)
