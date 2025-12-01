a62a31fd586f95649755b710a8d5b6056059be18
Kabilkirithik <kydykabil@gmail.com>
2025-12-01 14:17:52 +0530

    Calculate.py modified

--- calculate.py
+++ calculate.py
@@ -1,6 +1,6 @@
 import json 
 import input
-
+import math
 with open("customer_data.json") as f:
     statement = json.load(f)
 
@@ -19,8 +19,14 @@ def calculate(statement):
     avg_spend_ratio = (avg_month_spend/avg_month_income) * 100
     avg_transaction_amount= total_transacrtion_amount
     for trans in statement["transaction"]:
-        mean_diff = avg_transaction_amount - trans["amount"]
+        mean_diff = (avg_transaction_amount - trans["amount"])**2
+        sum_of_sq+=mean_diff
+        variance = sum_of_sq/no_of_transaction
+        standard_deviation = math.sqrt(variance)
+    for trans in statement["transaction"]:
+        pass
         
+
 
    
 
